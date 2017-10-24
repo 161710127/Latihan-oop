eea35e8b77f7fc3d3508c425e59ad18605539405
161710127 <161710127@smkassalaambandung.sch.id>
2017-10-24 08:54:26 +0700

    Git hub sudah ada

--- bangundatar.php
+++ bangundatar.php
@@ -3,32 +3,34 @@
 class bangundatar{
 	public $bil1,$bil2;
 	function set_nilai_persegi($sisi){
-		$this -> bil1 = $sisi;
+		$this ->bil1 = $sisi;
 	}
 	function get_nilai_persegi_luas(){
-		return $this -> bil1  * $this -> bil1;
+		return $this->bil1  *  $this->$bil1;
 	}
 	function set_nilai_persegi_panjang($panjang,$lebar){
-		$this -> bil1 = panjang:
-		$this -> bil2 = lebar;
+		$this ->bil1 =$panjang;
+		$this ->bil2 =$lebar;
 	}
 	function get_nilai_persegi_panjang_luas(){
-		return $this -> bil1 * $this -> bil2;
+		return $this ->bil1 * $this ->$bil2;
 	}
 	function get_nilai_persegi_panjang_keliling(){
-		return $this -> bil1 + $this -> bil2 *2;
+		return $this ->bil1 + $this ->$bil2 *2;
 	}
 	function set_nilai_segitiga($alas,$tinggi){
-		$this -> bil1 = alas:
-		$this -> bil2 = tinggi;
+		$this ->bil1 =$alas;
+		$this ->bil2 =$tinggi;
 	}
 	function get_nilai_segitiga_luas(){
-		return $this -> bil1 * $this -> bil2;
+		return ($this ->bil1 * $this ->bil2)/2;
 	}
 }
-$bangundatar1 = new bangundatar;
-$bangundatar1 -> set_nilai(8,10);
-echo "Luas Persegi         :".$bangundatar1 -> get_nilai_persegi_luas().'<br>';
-echo "Luas Persegi Panjang :".$bangundatar1 -> get_nilai_persegi_panjang_luas().'<br>';
-echo "Luas Segitiga :".$bangundatar1 -> get_nilai_segitiga_luas().'<br>';
+$bangundatar = new bangundatar;
+$bangundatar -> set_nilai_persegi(10);
+echo "Luas Persegi :".$bangundatar ->get_nilai_persegi_luas().'<br>';
+$bangundatar -> set_nilai_persegi_panjang(8,10);
+echo "Luas Persegi Panjang :".$bangundatar -> get_nilai_persegi_panjang_luas().'<br>';
+$bangundatar -> set_nilai_segitiga(8,10);
+echo "Luas Segitiga :".$bangundatar -> get_nilai_segitiga_luas().'<br>';
 ?>
